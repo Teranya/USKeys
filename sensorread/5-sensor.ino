9615babdf9c54fbf932091422c0bfe215f903bdc
Tanvi C <tanvichhabra14@gmail.com>
2019-12-10 22:44:42 -0500

    Add files via upload

---
+++ sensorread/5-sensor.ino
@@ -0,0 +1,96 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN     13
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT  10
+
+// NeoPixel brightness, 0 (min) to 255 (max)
+#define BRIGHTNESS 50
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+int trigPin1=2;
+int echoPin1=3;
+
+int trigPin2=4;
+int echoPin2=5;
+
+int trigPin3=6;
+int echoPin3=7;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+   pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+   pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+
+
+   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+}
+
+void loop() {
+  long duration1, distance1;
+  digitalWrite(trigPin1, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin1, LOW);
+  duration1 = pulseIn(echoPin1, HIGH);
+  distance1 = (duration1/2) / 29.1;
+
+long duration2, distance2;
+  digitalWrite(trigPin2, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin2, LOW);
+  duration2 = pulseIn(echoPin2, HIGH);
+  distance2= (duration2/2) / 29.1;
+
+  long duration3, distance3;
+  digitalWrite(trigPin3, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin3, LOW);
+  duration3 = pulseIn(echoPin3, HIGH);
+  distance3= (duration3/2) / 29.1;
+
+  Serial.print(distance1);
+    Serial.print(",");
+Serial.print(distance2);
+    Serial.print(",");
+Serial.print(distance3);
+    Serial.println(",");
+    
+if (distance2 <100){ 
+  for(int i=0; i<LED_COUNT; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    strip.setPixelColor(i, strip.Color(0, 150, 0));
+
+    strip.show();   // Send the updated pixel colors to the hardware.
+
+}}else{
+} 
+for(int i=0; i<LED_COUNT; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+
+    strip.show();   // Send the updated pixel colors to the hardware. }delay(200);
+}}
